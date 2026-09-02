e8e7a21393567f740bdb18cb9d72493ade1cf6b4
Kevin Ramos <129549988+nivek45@users.noreply.github.com>
2026-09-02 14:31:58 -0300

    Update app.js

--- Atividade18/app.js
+++ Atividade18/app.js
@@ -1,1 +1,21 @@
+let express = require('express');
+let app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("<h1>Site FATEC Sorocaba</h1>")
+})
+app.get('/historia', (req, res) => {
+    res.render("secao/historia")
+})
+app.get('/cursos', (req, res) => {
+    res.render("secao/cursos")
+})
+app.get('/professores', (req, res) => {
+    res.render("secao/professores")
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
